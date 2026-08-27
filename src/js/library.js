5d21f4001fcfd748d1bf7fd6dabbdee4881361bf
Zachary Collins <colz5081@gmail.com>
2026-08-27 14:47:29 -0500

    ENH: implement crud methods
    Add new methods for getBookById, getAllBooks, getReadBooks, getUnreadBooks, deleteBook

--- src/js/library.js
+++ src/js/library.js
@@ -10,6 +10,39 @@ class Library {
         this.books.push(book);
         return book;
     }
+
+    getBookById(id) {
+        return this.books.find(book => book.id === id);
+    }
+
+    getAllBooks() {
+        return [...this.books];
+    }
+
+    getReadBooks() {
+        return this.books.filter(book => book.read);
+    }
+
+    getUnreadBooks() {
+        return this.books.filter(book => !book.read);
+    }
+
+    deleteBook(id) {
+        const bookIndex = this.books.findIndex(
+            book => book.id === id
+        );
+
+        if (bookIndex === -1) {
+            throw new Error("Book not found");
+        }
+
+        const [deletedBook] = this.tasks.splice(
+            bookIndex,
+            1,
+        );
+
+        return deletedBook;
+    }
 }
 
 export { Library };
